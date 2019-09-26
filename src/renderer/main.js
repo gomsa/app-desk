a43e5bd72c6d5cd40636c8d8a9636ebf427238d0
bigrocs <bigrocs@qq.com>
2019-09-26 15:51:31 +0800

    init

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
+import axios from 'axios'
 
-import 'normalize.css/normalize.css'// A modern alternative to CSS resets
-
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
@@ -10,51 +10,21 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import '@/styles/index.scss' // global css
 import '@/icons' // icon
 import '@/permission' // permission control
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
-
-Vue.use(ElementUI, { locale })
-
+Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+// set ElementUI lang to EN
+Vue.use(ElementUI, { locale })
+
+/* eslint-disable no-new */
 new Vue({
   components: { App },
   router,
   store,
   template: '<App/>'
 }).$mount('#app')
-console.log('printer')
-const escpos = require('escpos')
-console.log(escpos)
-// Select the adapter based on your printer type
-// const device = new escpos.USB()
-// const device  = new escpos.Network('localhost');
-const device = new escpos.Serial('LTP1')
-console.log(device)
-const options = { encoding: 'GB18030' /* default */ }
-// encoding is optional
-console.log(options)
-const printer = new escpos.Printer(device, options)
-console.log(printer)
-
-device.open(function() {
-  printer
-    .font('a')
-    .align('ct')
-    .style('bu')
-    .size(1, 1)
-    .text('The quick brown fox jumps over the lazy dog')
-    .text('敏捷的棕色狐狸跳过懒狗')
-    .barcode('1234567', 'EAN8')
-    .table(['One', 'Two', 'Three'])
-    .tableCustom([
-      { text: 'Left', align: 'LEFT', width: 0.33 },
-      { text: 'Center', align: 'CENTER', width: 0.33 },
-      { text: 'Right', align: 'RIGHT', width: 0.33 }
-    ])
-    .qrimage('https://github.com/song940/node-escpos', function(err) {
-      console.log(err)
-    })
-})
